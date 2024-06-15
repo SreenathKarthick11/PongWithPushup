50a24eae0b8cf8512a50722439ec33a6de722a0a
SreenathKarthick11 <165347249+SreenathKarthick11@users.noreply.github.com>
2024-06-15 09:16:54 +0530

    Add files via upload

---
+++ phone-webcam.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+# camera and videcapture
+address1="http://100.75.141.213:8080/video"
+video = cv2.VideoCapture(0) # number stands for which web cam u want to use,if file name we can see the video
+video.open(address1)
+while True:
+    ret,frame = video.read() # ret returns false when camera is being used
+                           # frames gives the current frame
+
+    cv2.imshow("Frame",frame)
+
+    if cv2.waitKey(1) == ord("q"):# when u press key q for 1 milli sec the loop will break
+        break
+
+video.release() # release the camera from use
+cv2.destroyAllWindows()
